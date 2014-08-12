8b72b9a5f292b4abdb97508a06f208b427df22c4
James Robertson <james@debian.qbitx.com>
2014-08-12 10:53:01 +0100

    rewrote methods and sanitised the logic of the initial LCD settings.

--- lib/rpi_lcd16x2.rb
+++ lib/rpi_lcd16x2.rb
@@ -3,89 +3,47 @@
 # file: rpi_lcd16x2.rb
 
 # code originally copied from https://github.com/RobvanB/Ruby-RaspberryPi-LCD
+# instruction set reference https://en.wikipedia.org/wiki/HD44780_Character_LCD
 
 require 'wiringpi'
 
-T_MS = 1.0000000/1000000
-ON   = 1
-OFF  = 0
+T_MS = 0.000001
+
 
 class RpiLcd16x2
 
-  def initialize(string='hello world', p_rs: 11, p_en: 10, d4: 6, d5: 5, d6: 4, d7: 1)
+  def initialize(string='', p_rs: 11, p_en: 10, d4: 6, d5: 5, d6: 4, d7: 1)
 
     @p_rs, @p_en, @d4, @d5, @d6, @d7 = p_rs, p_en, d4, d5, d6, d7
-
-    @char_count = 0
 
     Wiringpi.wiringPiSetup
 
     # Set all pins to output mode
     [p_rs, p_en, d7, d6, d5, d4].each {|pin,val| Wiringpi.pinMode(pin, 1)}
 
-    init_display()
-    sleep T_MS * 10
-    lcd_display(ON, ON, OFF)
-    set_entry_mode()
-    cls
-    text string
-
+    function_set # sets the display to use 4-bit instructions
+    cursor_off
+    print string unless string.empty?
   end
 
   # Clear all data from screen
   #
-  def cls()
-    [0,1].each {|x| write_command d4: x}
-  end
+  def cls() cmd d4: 1                       end
 
-  def text(s)
-    # If the string is longer than 16 char - split it.
-    if (s.length > 16)
-      s.split(" ").each do |x|
-        print_text(x)
-        next_line()
-        sleep 1
-      end
-    else
-      print_text(s)
-    end
-    next_line()
-  end
+  def cursor(direction=1) cmd d7: 1, d6: direction, d5: nil, d4: nil end
+  def cursor_off()  cmd d7: 1, d6: 1, d5: 0 end
+  def cursor_on()   cmd d7: 1, d6: 1, d5: 1 end
+  def display_off() cmd d7: 1               end
+  def display_on()  cmd d7: 1, d6: 1        end
 
-  private
-
-  # Indicate to LCD that command should be 'executed'
+  # Sets cursor move direction (I/D); specifies to shift the display (S).
+  # I/D - increment = 1 or decrement = 0
+  # S - 0 = no display shift, 1 = display shift
+  # e.g. 0  0 0 0 1 D C B
   #
-  def pulse_enable()
-    [0,1,0].each {|x| Wiringpi.digitalWrite(@p_en, x); sleep T_MS * 10}
-  end
-
-  # Turn on display and cursor
-  #
-  # 1 = 0n
-  # 1 = Cursor on, 0 = Cursor off
-  # 1 = Block, 0 = Underline cursor
-  #
-  def lcd_display(display, cursor, block)
-
-    write_command
-    write_command d7: 1, d6: display, d5: cursor, d4: block
-  end
-
-  def write_command(rs: 0, d7: 0, d6: 0, d5: 0, d4: 0)
-
-    [@p_rs, @d7, @d6, @d5, @d4,].zip([rs,d7,d6,d5,d4])\
-                          .each {|pin,val| Wiringpi.digitalWrite(pin, val)}
-    pulse_enable()
-  end
-
-  # Entry mode set: move cursor to right after each DD/CGRAM write
-  #
-  def set_entry_mode()
-
-    write_command
-    write_command d6: 1, d5: 1
-    sleep T_MS
+  def entry(direction=1)
+    write_command rs: 1, d7: 0, d6: 0,         d5: 0,   d4: 1
+    write_command rs: 1, d7: 0, d6: direction, d5: nil, d4: nil
   end
 
   # Write data to CGRAM/DDRAM
@@ -94,61 +52,56 @@ class RpiLcd16x2
 
     write_command rs: 1, d7: a[0], d6: a[1], d5: a[2], d4: a[3]
     write_command rs: 1, d7: a[4], d6: a[5], d5: a[6], d4: a[7]
-    @char_count += 1
   end
 
-  # Set function to 4 bit operation
-  #
-  def init_display()
+  def home()        cmd d5: 1               end
 
-    3.times { sleep 42 * T_MS; write_command d5: 1, d4: 1 }
 
-    # Function set to 4 bit
-    2.times { write_command d5: 1}
-
-    # Set number of display lines
-
-    # D7: N = 0 = 1 line display
-    # D6: F = 0 = 5x8 character font
-
-    write_command d7: 1; sleep T_MS
-
-    # Display Off (2 blocks)
-    write_command;       sleep T_MS
-    write_command d7: 1; sleep T_MS
-
-    # Display clear (2 blocks)
-    write_command;       sleep T_MS
-    write_command d4: 1; sleep T_MS
-
-    # Entry mode set"
-    write_command;       sleep T_MS
-
-    # D5: 1 = Increment by 1
-    # D4: 0 = no shift
-
-    write_command d6: 1, d5: 1, d4: 1
+  def print(raw_s)
+    cls
+    s = raw_s.split(/\n/).map {|x| x.ljust(40)}.join
+    print_text s
   end
-  
+
+  alias text print
+
   def print_text(s)
-    s.each_byte.to_a.map{|x| lcd_write ("%08b" % x).each_char.to_a.map(&:to_i)}
+
+    s.each_byte.to_a.each do |x|
+      lcd_write ("%08b" % x).each_char.to_a.map(&:to_i)
+    end
   end
 
-  # Display automatically goes to next line when we hit 40 chars
-  #
-  def next_line()
-    
-    fill_str  = " "
-    fill_cntr = 1
+  protected
 
-    while (@char_count + fill_cntr < 40)
-      fill_str += " "
-      fill_cntr += 1
+  def cmd(h) write_command; write_command h;  sleep 0.01  end
+
+  # Sets interface data length (DL), number of display line (N), 
+  #                                                and character font (F).
+  # DL - 0 = 4-bit interface, 1 = 8-bit interface
+  # N - 0 = 1/8 or 1/11 duty (1 line), 1 = 1/16 duty (2 lines)
+  # F - 0 = 5×8 dots, 1 = 5×10 dots
+  # e.g.  0  0  1 DL  N   F  *   *
+  #      d7 d6 d5 d4  d7 d6 d5 d4
+  def function_set()
+    write_command rs: 1, d7: 0, d6: 0, d5: 1, d4: 0
+    write_command rs: 1, d7: 1, d6: 0, d5: nil, d4: nil
+  end
+
+  def write_command(rs: 0, d7: 0, d6: 0, d5: 0, d4: 0)
+
+    [@p_rs, @d7, @d6, @d5, @d4,].zip([rs,d7,d6,d5,d4]).each do |pin,val| 
+      Wiringpi.digitalWrite(pin, val) if val
     end
+    pulse_enable()
+  end
 
-    print_text(fill_str)
+  private
 
-    @char_count = 0
+  # Indicate to LCD that command should be 'executed'
+  #
+  def pulse_enable()
+    [0,1,0].each {|x| Wiringpi.digitalWrite(@p_en, x); sleep T_MS * 10}
   end
 
 end
